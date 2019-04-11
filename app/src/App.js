52da44f26062dcfb30e99dc2f92493a916d89287
Neto <programador.junior.2@corp.agenciaacerte.com>
2019-04-11 17:39:29 -0300

    Desafio básico

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import BtnBotcamp from "./components/BtnBotcamp";
+import LogoBotcamp from "./components/LogoBotcamp";
 
 class App extends Component {
   render() {
-    return <BtnBotcamp/>
+    return [ <LogoBotcamp/>, <BtnBotcamp/> ]
   }
 };
 
---
+++ app/src/components/LogoBotcamp/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LogoBotcamp = () => (
+  <img src="images/botcamp.svg" alt="Logo Botcamp"/>
+);
+
+export default LogoBotcamp;
