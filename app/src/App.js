d9c920176c459c81ee56caa971b97814d856a8b2
Neto <programador.junior.2@corp.agenciaacerte.com>
2019-04-22 10:08:23 -0300

    Cria diretório pages e isola páginas

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from "react";
 
-import LoginWrapper from "./components/LoginWrapper";
-import BtnBotcamp from "./components/BtnBotcamp";
-import LogoBotcamp from "./components/LogoBotcamp";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 import "./styles/reset.css";
 
 class App extends Component {
   render() {
-    return(
-       <LoginWrapper>
-          <LogoBotcamp/>
-          <BtnBotcamp content= "Entrar"/>
-       </LoginWrapper>
-    )
+    return <Login/>;
   }
-};
+}
 
 export default App;
